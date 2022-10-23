fb4a92d5cbe3e17adbec83a00a1ebc552d1154af
BobSaidHi <47067448+BobSaidHi@users.noreply.github.com>
2022-10-22 17:08:06 -0700

    Added logging, updated spelling config

---
+++ logger.py
@@ -0,0 +1,67 @@
+# Module:  [root]
+# SubComponent:   logger
+# COMPONENT:  Mech Command Server
+# Project:    Mech Warfare 2022
+
+# Author:    @BoBSaidHi - https://github.com/BobSaidHi
+# Version:   0.0.0.1.2
+# Version Name:  INDEV 1.2
+# Updated:   2022-10-22T5:00 PM PST
+
+# From FRC Scouting Suite (reboot) / FRC Scouting Server / api-core / basic
+
+# This Source Code Form is subject to the terms of the Mozilla Public
+#   License, v. 2.0. If a copy of the MPL was not distributed with this
+#   file, You can obtain one at http://mozilla.org/MPL/2.0/.
+
+
+# Standard Imports
+from  datetime import datetime as datetime
+import logging
+from sys import exit as sys_exit
+from time import sleep
+import os
+
+# Start Logger
+logger = logging.getLogger("MechWarfareCommandServer")
+logger.setLevel(logging.DEBUG)
+
+def CreateLoggingFh():
+    Current_time = datetime.now().strftime('%Y-%m-%d__%H-%M-%S')
+    fh = logging.FileHandler('logs/MechWarfareCommandServer-' + Current_time + '.log')
+    fh.setLevel(logging.DEBUG)
+    formatterFh = logging.Formatter('%(asctime)s - %(pathname)s - %(name)s - %(levelname)s - %(message)s', datefmt='%m-%d-%Y %I:%M:%S %p')
+    fh.setFormatter(formatterFh)
+    logger.addHandler(fh)
+    logger.debug("Log to file handler created!")
+try:
+    CreateLoggingFh()
+except(FileNotFoundError):
+    os.mkdir('logs')
+    CreateLoggingFh()
+    logger.info("Couldn't find log directory and created a new one instead.")
+    pass
+
+# Log to the console/ terminal
+ch = logging.StreamHandler()
+ch.setLevel(logging.INFO)
+formatterCh = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s', datefmt='%m-%d-%Y %I:%M:%S %p')
+ch.setFormatter(formatterCh)
+logger.addHandler(ch)
+logger.debug("Log to console handler created!")
+logger.info("Imported most modules and started Logger.")
+
+# @deprecated Not needed for this program
+def ModuleNotFound(missingModule, module):
+    logger.critical("Module not found error.")
+    print("'" + missingModule + " module not found but required for " + module + " .  Would you like to run setup and install it now?  (Y/n)")
+    answer = input().lower()
+    if answer == 'y':
+        logger.critical("Not Implemented")
+        #Run Setup
+    elif answer == 'n':
+        logger.critical("Exiting Program...")
+        sys_exit(0)
+    else:
+        logger.warning("Invalid input.  Try again...")
+        ModuleNotFound(missingModule)
--- main.py
+++ main.py
@@ -4,38 +4,59 @@
 
 # Imports
 import socket
+import logging
+import logger # Apparently importing is enough to run the setup script
+
+# Start Logger
+logger = logging.getLogger("MechWarfareCommandServer")
+logger.setLevel(logging.DEBUG)
+
+logger.info("Logger started.")
 
 # Config
-#TARGET_IP = 0.0.0.0' # Use '0.0.0.0' for all or figure out ip address of target
+logger.debug("Starting configuration")
+#TARGET_IP = '0.0.0.0' # Use '0.0.0.0' for all or figure out IP address of target
 #PORT = 8090
 TARGET_IP = '0.0.0.0' # Use '0.0.0.0' for all
 PORT = 8091
 
+if(TARGET_IP == "0.0.0.0"):
+    logger.warning("Accepting sockets from all IPs!")
+
+logger.info("TARGET_IP: " + TARGET_IP)
+logger.info("PORT: " + str(PORT))
+
 # Setup
+logger.debug("Starting socket setup")
 commsSocket = socket.socket()
 #commsSocket.bind((IP, PORT))
 commsSocket.bind(("0.0.0.0", 8091)) # Use '0.0.0.0' for all or find out target IP
 commsSocket.listen(0)
+logger.info("Socket setup compete")
 
 # Server Loop
 while True:
- 
+    logger.info("Waiting for connection.")
+
+    # Connect
     client, addr = commsSocket.accept()
-    print(client)
-    print(addr)
+    logger.info("Client: " + client)
+    logger.info("Address: " + addr)
+    logger.info("Connection accepted.")
     # client handling code
 
     # Receive
     while True:
+        logger.info("Waiting for content.")
         content = client.recv(32)
- 
+        
         if len(content) == 0:
-           break
+            logger.critical("Content empty")
+            break
  
         else:
-            print(content)
+            logger.inof("Content:" + content)
 
-        print("Closing connection")
-        client.close()
-
+    logger.warning("Closing connection.")
+    client.close()
 
